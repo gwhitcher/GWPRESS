4b2b78c136402262e0f08c5be5708b16a709055b
George Whitcher <george@georgewhitcher.com>
2017-04-21 10:37:28 -0400

    0.51

--- gw-content/themes/gwpress/footer.php
+++ gw-content/themes/gwpress/footer.php
@@ -3,6 +3,18 @@
 <!-- FOOTER -->
 <div class="container">
 <footer>
+    <div class="advertisement">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Responsive -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-1862231357641748"
+             data-ad-slot="1935611714"
+             data-ad-format="auto"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
     <p class="pull-right"><a href="#">Back to top</a></p>
     <p>&copy; <?php echo date("Y"); ?> <?php echo SITE_TITLE; ?> - <?php echo SITE_HEADLINE; ?></p>
 </footer>
